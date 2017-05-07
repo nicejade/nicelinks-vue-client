3270ae67f321dfba25b7786c470c17239a1f2cde
jeffjade <jeffygisgreat@gmail.com>
2017-05-07 18:32:17 +0800

    fix(config): get rid of an unnecessary judgment🐛

--- src/assets/icons/index.js
+++ src/assets/icons/index.js
@@ -3,7 +3,6 @@ const files = require.context('.', true, /\.svg$/)
 const modules = {}
 
 files.keys().forEach((key) => {
-  if (key === './index.js') return
   modules[key.replace(/(\.\/|\.svg)/g, '')] = files(key)
 })
 export default modules

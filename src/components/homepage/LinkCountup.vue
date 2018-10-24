441a1feee856b82a279cbe791d6088fbcb696d02
JadeYang(杨琼璞) <jeffygisgreat@gmail.com>
2018-10-25 00:20:50 +0800

    style: update   style 💄

--- src/components/homepage/LinkCountup.vue
+++ src/components/homepage/LinkCountup.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="countup-area">
+    <div class="top-crescent" v-if="!isMobile"></div>
     <h2 class="medium-font">{{ $t('countupText') }}</h2>
     <CountUp
       id="countup-number"
@@ -87,16 +88,25 @@ export default {
 @import './../../assets/scss/mixins.scss';
 
 .countup-area{
+  position: relative;
+  width: 100%;
+  height: $section-height;
+  @include flex-box-center(column);
+  color: $black;
+  background: $white-grey;
+  background: -webkit-linear-gradient(to top, $white-grey, #eef2f3);
+  background: linear-gradient(to top, $white-grey, #eef2f3);
+  .top-crescent{
+    position: absolute;
+    top: - $crescent-height / 2;
+    width: 100%;
+    height: $crescent-height;
+    clip-path: ellipse(64% 50% at 50% 50%);
+    background-color: $white;
+  }
   .countup-number{
     display: block;
     margin-top: 10px;
   }
-  color: $black;
-  width: 100%;
-  height: $section-height;
-  @include flex-box-center(column);
-  background: #f4f6fa;
-  background: -webkit-linear-gradient(to top, #f4f6fa, #eef2f3);
-  background: linear-gradient(to top, #f4f6fa, #eef2f3);
 }
 </style>

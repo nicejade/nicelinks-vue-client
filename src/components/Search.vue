60cfd6a417d04fef0ee0d3abefda506437193bc0
JadeYang(杨琼璞) <jeffygisgreat@gmail.com>
2019-01-10 12:39:36 +0800

    feat: perfect site search ✨ #29

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -1,8 +1,10 @@
 <template>
   <el-autocomplete
     popper-class="search-autocomplete"
-    select-when-unmatched="true"
+    placement="bottom-start"
+    :select-when-unmatched="isSelectWhenUnmatched"
     :trigger-on-focus="isTriggerFocus"
+    :popper-append-to-body="isPopperToBody"
     v-model="keyword"
     :fetch-suggestions="handleFetchNiceLinks"
     :placeholder="$t('searchYourWant')"
@@ -16,7 +18,7 @@
     </i>
     <template slot-scope="{ item }">
       <p class="item-title" v-html="styleForTitle(item)"></p>
-      <span class="item-desc" v-html="styleForDesc(item)"></span>
+      <div class="item-desc" v-html="styleForDesc(item)"></div>
     </template>
   </el-autocomplete>
 </template>
@@ -29,7 +31,10 @@ export default {
   data () {
     return {
       keyword: '',
-      isTriggerFocus: true
+      keywordBackup: '',
+      isSelectWhenUnmatched: true,
+      isTriggerFocus: true,
+      isPopperToBody: true
     }
   },
 
@@ -50,8 +55,10 @@ export default {
 
   methods: {
     handleFetchNiceLinks (queryString, callback) {
-      if (queryString.trim() === '') {
-        return this.$apis.getRandomLinks({ size: 6 }).then(result => {
+      this.keywordBackup = queryString
+      if (queryString === '' && queryString.trim() === '') {
+        const defSize = this.$isMobileScreen() ? 8 : 16
+        return this.$apis.getRandomLinks({ size: defSize }).then(result => {
           callback(result)
         }).catch(error => {
           this.$message.error(`${error}`)
@@ -69,22 +76,35 @@ export default {
     },
 
     handleSearchSelect (item) {
-      this.$router.push(`/post/${item._id}`)
+      if (!item || !item._id) return
+      const keyword = this.keywordBackup || 'srandom'
+      const stype = item.stype || 'srandom'
+      const paramStr = `?keyword=${keyword}&type=${stype}`
+      this.$router.push(`/post/${item._id}${paramStr}`)
     },
 
     handleIconClick () {
     },
 
     handleSearchFocus () {
-      if (!this.$isMobileScreen()) {
-        document.querySelector('.el-input__inner').style.width = '395px'
+      if (this.$isMobileScreen()) {
+        if (this.$util.isAndroidSystem()) {
+          document.querySelector('.search-autocomplete').style.display = 'none'
+          setTimeout(() => {
+            // this.$nextTick(() => {})
+            document.querySelector('.search-autocomplete').style.display = 'block'
+            document.querySelector('.search-autocomplete').style.top = '110px'
+          }, 360)
+        }
+        return
       }
+      const expandedWidth = window.innerWidth > 1280 ? 666 : 521
+      document.querySelector('.el-input__inner').style.width = `${expandedWidth}px`
     },
 
     handleSearchBlur () {
-      if (!this.$isMobileScreen()) {
-        document.querySelector('.el-input__inner').style.width = '200px'
-      }
+      if (this.$isMobileScreen()) return
+      document.querySelector('.el-input__inner').style.width = '221px'
     },
 
     styleForTitle (item) {
@@ -93,11 +113,14 @@ export default {
 
     styleForDesc (item) {
       const tempDesc = (item && item.stype)
-        ? item[item.stype].replace(this.keyword, `<i class="keyword">${this.keyword}</i>`)
+        ? item[item.stype]
         : item.review || item.desc
-      return marked(tempDesc, {
+      let niceDesc = marked(tempDesc, {
         sanitize: true
       })
+      niceDesc = this.$util.filterHtmlTag(niceDesc)
+      niceDesc = this.$util.sliceToAheadTarget(niceDesc, this.keyword)
+      return niceDesc.replace(this.keyword, `<i class="keyword">${this.keyword}</i>`)
     }
   },
 
@@ -117,8 +140,8 @@ export default {
 .el-autocomplete-suggestion {
   .el-scrollbar {
     .el-autocomplete-suggestion__wrap {
-      height: 30rem;
-      min-height: 30rem;
+      height: 539px;
+      min-height: 539px;
     }
     .el-autocomplete-suggestion__list li {
       border-bottom: 1px dashed $border-grey;
@@ -126,19 +149,60 @@ export default {
   }
 }
 
-.search-autocomplete {
+@media (min-width: $large-screen) {
+  .el-autocomplete-suggestion {
+    .el-scrollbar {
+      .el-autocomplete-suggestion__wrap {
+        height: 677px;
+        min-height: 677px;
+      }
+    }
+  }
+}
+@media screen and (max-width: $mobile-screen) {
+  .el-autocomplete-suggestion {
+    top: 110px !important;
+    .el-scrollbar {
+      .el-autocomplete-suggestion__wrap {
+        height: 34rem;
+        min-height: 34rem;
+      }
+    }
+  }
+}
+@media screen and (max-width: $tiny-mobile-screen) {
+  .el-autocomplete-suggestion {
+    top: 110px !important;
+    .el-scrollbar {
+      .el-autocomplete-suggestion__wrap {
+        height: 28.6rem;
+        min-height: 28.6rem;
+      }
+    }
+  }
+}
+
+.search-autocomplete .el-autocomplete-suggestion__wrap{
   li {
-    padding: 10px;
-    .item-title{
+    padding: 0 15px;
+    line-height: 18px;
+    .item-title {
       font-size: $font-medium;
       color: $black;
       text-overflow: ellipsis;
       overflow: hidden;
+      line-height: 18px;
+      padding-top: 10px;
     }
-    .item-desc{
+    .item-desc {
+      display: inline-block;
+      line-height: 18px;
       padding-top: 5px;
+      padding-bottom: 10px;
       color: $silver-grey;
       font-size: $font-small;
+      text-overflow: ellipsis;
+      overflow: hidden;
     }
   }
 }

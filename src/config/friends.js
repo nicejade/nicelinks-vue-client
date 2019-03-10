67c25fac64565cb5efd398510ab0ac51c0a3f0ee
JadeYang(杨琼璞) <jeffygisgreat@gmail.com>
2019-03-10 23:44:01 +0800

    feat: update friends config ✨

--- src/config/friends.js
+++ src/config/friends.js
@@ -1,17 +1,22 @@
 export default [
   {
-    name: '静晴轩别苑',
-    label: 'nice.nicejade',
-    path: 'https://nice.lovejade.cn'
+    name: '静轩之别苑',
+    label: 'jeffjade',
+    path: 'https://quickapp.lovejade.cn/'
   },
   {
     name: '晚晴幽草轩',
-    label: 'jeffjade',
-    path: 'https://jeffjade.com'
+    label: 'jeffjade.com',
+    path: 'https://jeffjade.com/'
+  },
+  {
+    name: '静晴轩别苑',
+    label: 'nice.nicejade',
+    path: 'https://nice.lovejade.cn/'
   },
   {
     name: '天意人间舫',
     label: 'blog.nicejade',
-    path: 'https://blog.lovejade.cn'
+    path: 'https://blog.lovejade.cn/'
   }
 ]

b983c7d0007620fce0e35e5cff91482c48966014
JadeYang(杨琼璞) <jeffygisgreat@gmail.com>
2021-05-13 21:59:10 +0800

    :sparkles: inject google ads

--- build/build.js
+++ build/build.js
@@ -29,22 +29,27 @@ shell.rm('-rf', assetsPath)
 shell.mkdir('-p', assetsPath)
 shell.config.silent = true
 shell.cp('-R', 'static/*', assetsPath)
+shell.cp('./ads.txt', assetsRoot)
 shell.config.silent = false
 
 webpack(webpackConfig, function (err, stats) {
   spinner.stop()
   if (err) throw err
-  process.stdout.write(stats.toString({
-    colors: true,
-    modules: false,
-    children: false,
-    chunks: false,
-    chunkModules: false
-  }) + '\n\n')
+  process.stdout.write(
+    stats.toString({
+      colors: true,
+      modules: false,
+      children: false,
+      chunks: false,
+      chunkModules: false,
+    }) + '\n\n'
+  )
 
   console.log(chalk.cyan('  Build complete.\n'))
-  console.log(chalk.yellow(
-    '  Tip: built files are meant to be served over an HTTP server.\n' +
-    '  Opening index.html over file:// won\'t work.\n'
-  ))
+  console.log(
+    chalk.yellow(
+      '  Tip: built files are meant to be served over an HTTP server.\n' +
+        "  Opening index.html over file:// won't work.\n"
+    )
+  )
 })

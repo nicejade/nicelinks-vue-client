bab5521a5871cabdf173a1aa7b7e7f2f32e7b0d4
JadeYang(杨琼璞) <jeffygisgreat@gmail.com>
2021-05-19 23:15:04 +0800

    :sparkles: optimize verify url

--- src/helper/util.js
+++ src/helper/util.js
@@ -187,7 +187,7 @@ export default {
   },
 
   isLegalUrl(str) {
-    let pattern = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\\+.~#?&//=]*)/
+    let pattern = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\\+~#=]{2,256}\.[a-z]{2,9}\b([-a-zA-Z0-9@:%_\\+.~#?&//=]*)/
     return pattern.test(str)
   },
 

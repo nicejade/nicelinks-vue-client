0ee24a07bd641302abe1d6eab4e8785506d577dc
JadeYang(杨琼璞) <jeffygisgreat@gmail.com>
2022-01-31 21:59:38 +0800

    :sparkles: update console log content

--- src/config/constant.js
+++ src/config/constant.js
@@ -1,6 +1,6 @@
 export const STORAGE_PREFIX = 'ns-storage'
 
-export const AUTO_DIALOG = 'auto-dialog'
+export const AUTO_DIALOG = `auto-dialog-${new Date().getFullYear()}`
 
 export const REPORT_PATH =
   'https://nicelinks.site/redirect?url=https://support.qq.com/products/340357&isauto=true'

3b3622ab8c8fb084b9e175523ef40bfa07ef8b28
JadeYang(杨琼璞) <jeffygisgreat@gmail.com>
2022-02-06 17:36:22 +0800

    :lipstick: style detail optimization

--- src/locales/zh.js
+++ src/locales/zh.js
@@ -23,7 +23,7 @@ export default {
   reviewStr: '推荐语：',
   warmReminder: '温馨提示',
   niceLinksStr: '倾城之链',
-  searchYourWant: '搜您想要',
+  searchYourWant: '虎年大吉，搜您想要',
 
   all: '全部',
   skill: '技术客栈',

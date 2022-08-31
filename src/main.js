687a5fb063bf01a5c204985d7cc3a5ea8f4a1393
JadeYang(杨琼璞) <jeffygisgreat@gmail.com>
2022-08-31 21:57:09 +0800

    :fire: remove useless code

--- src/main.js
+++ src/main.js
@@ -6,9 +6,12 @@ import router from './router'
 import store from './store/'
 
 const version = require('../package.json').version
-console.log(
-  `倾城之链：“倾心缔造，痴心为你”，您当前运行的版本为：%c${version}`,
-  'font-weight: 600;color: #20a0ff'
+const log = console.info
+log(
+  `%c 倾城之链%c：“倾心缔造，痴心为你”，您当前运行的版本为：%c ${version}`,
+  'font-weight: 600;color: #ff6768',
+  'color: #17223b',
+  'color: #20a0ff'
 )
 
 const isProductionFlag = process.env.NODE_ENV === 'production'

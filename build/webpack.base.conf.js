a3d9f0754b3ac88e7639f398749bdbf60c1be554
JadeYang(杨琼璞) <jeffygisgreat@gmail.com>
2022-09-25 17:16:09 +0800

    :arrow_up: upgrade vue to 2.7.10

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -11,6 +11,7 @@ const HappyPack = require('happypack')
 const os = require('os')
 const happyThreadPool = HappyPack.ThreadPool({ size: os.cpus().length })
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const { VueLoaderPlugin } = require('vue-loader')
 
 const env = process.env.NODE_ENV
 
@@ -19,15 +20,13 @@ const resolve = (dir) => {
 }
 
 const cssLoader = new MiniCssExtractPlugin({
-  use: [
-    'happypack/loader?id=happy-css'
-  ]
+  use: ['happypack/loader?id=happy-css'],
 })
 
 // inject happypack accelerate packing for vue-loader @17-08-18
 Object.assign(vueLoaderConfig.loaders, {
   js: 'happypack/loader?id=happy-babel-vue',
-  css: cssLoader
+  css: cssLoader,
 })
 
 function createHappyPlugin(id, loaders) {
@@ -36,7 +35,7 @@ function createHappyPlugin(id, loaders) {
     loaders: loaders,
     threadPool: happyThreadPool,
     // make happy more verbose with HAPPY_VERBOSE=1
-    verbose: process.env.HAPPY_VERBOSE === '1'
+    verbose: process.env.HAPPY_VERBOSE === '1',
   })
 }
 
@@ -44,34 +43,32 @@ module.exports = {
   entry: {
     app: './src/main.js',
     lodash: ['lodash'],
-    element: ['element-ui']
+    element: ['element-ui'],
   },
   mode: env === 'production' ? 'production' : 'development',
   output: {
     path: config.build.assetsRoot,
     filename: '[name].js',
-    publicPath: process.env.NODE_ENV === 'production'
-      ? config.build.assetsPublicPath
-      : config.dev.assetsPublicPath
+    publicPath:
+      process.env.NODE_ENV === 'production'
+        ? config.build.assetsPublicPath
+        : config.dev.assetsPublicPath,
   },
   resolve: {
     extensions: ['.js', '.vue', '.json'],
-    modules: [
-      resolve('src'),
-      resolve('node_modules')
-    ],
+    modules: [resolve('src'), resolve('node_modules')],
     alias: {
-      'vue$': 'vue/dist/vue.min.js',
-      'src': resolve('src'),
-      'assets': resolve('src/assets'),
-      'components': resolve('src/components'),
-      'config': resolve('src/config'),
-      'helper': resolve('src/helper'),
-      'views': resolve('src/views'),
-      'mixins': resolve('src/mixins'),
-      'partials': resolve('src/partials'),
-      'store': resolve('src/store')
-    }
+      vue$: 'vue/dist/vue.min.js',
+      src: resolve('src'),
+      assets: resolve('src/assets'),
+      components: resolve('src/components'),
+      config: resolve('src/config'),
+      helper: resolve('src/helper'),
+      views: resolve('src/views'),
+      mixins: resolve('src/mixins'),
+      partials: resolve('src/partials'),
+      store: resolve('src/store'),
+    },
   },
   module: {
     noParse: /node_modules\/(element-ui\.js)/,
@@ -80,31 +77,31 @@ module.exports = {
         test: /\.svg$/,
         enforce: 'pre',
         loader: 'svgo-loader?' + JSON.stringify(svgoConfig),
-        include: /assets\/icons/
+        include: /assets\/icons/,
       },
       {
         test: /\.vue$/,
         loader: 'vue-loader',
         options: vueLoaderConfig,
         include: [resolve('src')],
-        exclude: /node_modules\/(?!(autotrack))|vendor\.dll\.js/
+        exclude: /node_modules\/(?!(autotrack))|vendor\.dll\.js/,
       },
       {
         test: /\.js[x]?$/,
         exclude: /node_modules/,
         include: [resolve('src')],
-        loader: 'happypack/loader?id=happy-babel-js'
+        loader: 'happypack/loader?id=happy-babel-js',
       },
       // inject loader for @waline/dist
       {
         test: /\.js[x]?$/,
         include: [/node_modules\/@waline/],
-        loader: 'babel-loader'
+        loader: 'babel-loader',
       },
       {
         test: /\.svg$/,
         loader: 'happypack/loader?id=happy-svg',
-        include: [/assets\/icons/, /node_modules\/mavon-editor/]
+        include: [/assets\/icons/, /node_modules\/mavon-editor/],
       },
       {
         test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
@@ -113,56 +110,59 @@ module.exports = {
         exclude: /assets\/icons/,
         query: {
           limit: 8192,
-          name: utils.assetsPath('img/[name].[hash:7].[ext]')
-        }
+          name: utils.assetsPath('img/[name].[hash:7].[ext]'),
+        },
       },
       {
         test: /\.(woff2?|eot|woff|ttf|otf)(\?.*)?$/,
         loader: 'url-loader',
         query: {
           limit: 8192,
-          name: utils.assetsPath('fonts/[name].[hash:7].[ext]')
-        }
-      }
-    ]
+          name: utils.assetsPath('fonts/[name].[hash:7].[ext]'),
+        },
+      },
+    ],
   },
   // externals中：key 是 require 的包名，value 是全局的变量。
   externals: {
     // 'element-ui': 'ElementUI',
     // 'vue': 'Vue',
-    'babel-polyfill': 'window'
+    'babel-polyfill': 'window',
   },
   plugins: [
     new ProgressBarPlugin({
-      format: '  build [:bar] ' + chalk.green.bold(':percent') + ' (:elapsed seconds)'
+      format: '  build [:bar] ' + chalk.green.bold(':percent') + ' (:elapsed seconds)',
     }),
     new webpack.DllReferencePlugin({
       context: path.resolve(__dirname, '..'),
-      manifest: require('./vendor-manifest.json')
+      manifest: require('./vendor-manifest.json'),
     }),
+    new VueLoaderPlugin(),
     new webpack.LoaderOptionsPlugin({
       options: {
         postcss: [
           autoprefixer({
-            browsers: ['last 2 version']
-          })
-        ]
-      }
+            browsers: ['last 2 version'],
+          }),
+        ],
+      },
     }),
     createHappyPlugin('happy-babel-js', ['babel-loader?cacheDirectory=true']),
     createHappyPlugin('happy-babel-vue', ['babel-loader?cacheDirectory=true']),
     createHappyPlugin('happy-svg', ['svg-sprite-loader']),
-    createHappyPlugin('happy-css', ['css-loader', 'vue-style-loader']),
+    createHappyPlugin('happy-css', ['css-loader']),
     new HappyPack({
-      loaders: [{
-        path: 'vue-loader',
-        query: {
-          loaders: {
-            scss: 'vue-style-loader!css-loader!sass-loader?indentedSyntax',
-            js: 'happypack/loader?id=happy-babel-vue'
-          }
-        }
-      }]
-    })
-  ]
+      loaders: [
+        {
+          path: 'vue-loader',
+          query: {
+            loaders: {
+              scss: 'css-loader!sass-loader?indentedSyntax',
+              js: 'happypack/loader?id=happy-babel-vue',
+            },
+          },
+        },
+      ],
+    }),
+  ],
 }
